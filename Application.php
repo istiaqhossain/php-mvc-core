4b6d7551da111be8c2c34bbf848309e6a03b961d
istiaqhossain <md.istiaqhossain1990@gmail.com>
2021-10-07 23:09:48 +0600

    Application Event Listeners added

--- Application.php
+++ Application.php
@@ -6,6 +6,11 @@ use istiaqhossain\phpmvc\db\Database;
 
 class Application 
 {
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+
+    protected array $eventListeners = [];
+    
     public static string $ROOT_DIR;
     public string $layout = 'default';
     public string $userClass;
@@ -42,6 +47,7 @@ class Application
 
     public function run()
     {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
         try {
             echo $this->router->resolve();
         } catch (\Exception $e) {
@@ -78,5 +84,18 @@ class Application
     public static function isGuest()
     {
         return !self::$app->user;
+    }
+
+    public function triggerEvent($eventName)
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
+    }
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName][] = $callback;
     }
 }
